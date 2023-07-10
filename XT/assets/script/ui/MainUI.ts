27a64ed628a6de3c2869b749b99f8242ff5a63f6
kaxifa <1813408689@qq.com>
2023-07-10 16:22:19 +0800

    更新demo

--- XT/assets/script/ui/MainUI.ts
+++ XT/assets/script/ui/MainUI.ts
@@ -24,16 +24,14 @@ export class MainUI extends WindowUI {
     private sp: Sprite = null;
 
     start() {
-        
+        xt.eventManager.on('a', this.cb2, this); //监听事件
+        xt.eventManager.once('a', this.cb2, this); //单次监听事件
+        xt.eventManager.off('a', this.cb1, this); //取消监听事件
+        xt.eventManager.emit('a'); //派发事件
+        xt.eventManager.offByTarget(this); //取消该对象上的所有事件
+
         xt.eventManager.on('a', this.cb1, this);
-
         xt.eventManager.emit('a', 1, 2, 3, 4, 5)
-        xt.warn('123')
-
-        this.registerBtnClickEvent(this.testBtn, this.cb2)
-        this.registerBtnClickEvent(this.testBtn, this.cb1)
-
-        this.loadSpriteFrame(this.sp, 'texture/1')
     }
 
     cb1(a, b, c, d, e) {
